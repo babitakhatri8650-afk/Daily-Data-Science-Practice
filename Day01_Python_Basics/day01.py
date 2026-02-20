1798c3a5e5ec0b873a052ae8a56606801b1f1c54
Babita Khatri <babitakhatri8650@gmail.com>
2026-02-20 14:00:02 -0300

    Create day01.py

---
+++ Day01_Python_Basics/day01.py
@@ -0,0 +1,12 @@
+print("Day 01 - Python Basics 🚀")
+
+name = "Babita"
+age = 23
+
+if age > 18:
+    print(name, "is eligible to vote")
+
+def square(x):
+    return x * x
+
+print("Square of 5:", square(5))
